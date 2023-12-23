ce56f97df380cc42ec591921c7ca255a7ed88f37
lilthanhderp <thanhnguyen.tnnn25@gmail.com>
2023-12-23 21:58:06 +0700

    thành sửa r nhé
    Footer dthanhblog2 bị lỗi nhé :((((((

--- js/app.js
+++ js/app.js
@@ -157,6 +157,51 @@ document.addEventListener("DOMContentLoaded", function () {
 
   type();
 })
+
+//DThanhJSHTML
+document.addEventListener("DOMContentLoaded", function () {
+  const options = {
+    strings: ['Đức Thành'],
+    typeSpeed: 180,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+  };
+
+  const multiTextElement = document.querySelector('.multi-textDT');
+  let currentTextIndex = 0;
+  let currentText = '';
+  let isDeleting = false;
+
+  function type() {
+    const fullText = options.strings[currentTextIndex];
+    if (isDeleting) {
+      currentText = fullText.substring(0, currentText.length - 1);
+    } else {
+      currentText = fullText.substring(0, currentText.length + 1);
+    }
+
+    multiTextElement.textContent = currentText;
+
+    let typeSpeed = options.typeSpeed;
+    if (isDeleting) {
+      typeSpeed /= 2; // Faster when deleting
+    }
+
+    if (!isDeleting && currentText === fullText) {
+      typeSpeed = options.backDelay;
+      isDeleting = true;
+    } else if (isDeleting && currentText === '') {
+      isDeleting = false;
+      currentTextIndex = (currentTextIndex + 1) % options.strings.length;
+    }
+
+    setTimeout(type, typeSpeed);
+  }
+
+  type();
+})
+
 // auto type words page ck
 
 document.addEventListener("DOMContentLoaded", function () {
